b1a762e58f10a96d1564a75309719b4f808aeb56
KingKyanon <53570893+KingKyanon@users.noreply.github.com>
2021-12-07 21:22:30 -0300

    Add files via upload

--- REVENGE/funciones.cpp
+++ REVENGE/funciones.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include <conio.h>
 using namespace std;
 
 void lanzardado12c (int RONDAS,int DADOS12, int mj [ ] [RONDAS+1] [DADOS12], int juego, int turno)
@@ -12,12 +11,49 @@ void lanzardado12c (int RONDAS,int DADOS12, int mj [ ] [RONDAS+1] [DADOS12], int
     }
 }
 
-void lanzardado6c (int RONDAS, int DADOSMAX, int mj [ ] [RONDAS ] [DADOSMAX], int dados, int juego, int turno)
+void lanzardado6c (int RONDAS, int DADOSMAX, int mj [ ] [RONDAS ] [DADOSMAX], int dadostock, int juego, int turno)
 {
-    for (int x = 0, x < dados, x++)
+    for (int x = 0, x < dadostock, x++)
     {
         mj [juego] [turno] [x] = 1 + rand () % 6;
     }
 }
 
-bool verificador (char opciones2)
+int verificadadoapasar (int dadostockj, int dadoapasar, int Vdadosasumar [], int RONDAS, int DADOSMAX, int mj [ ] [RONDAS ] [DADOSMAX], int juego, int turno, int valorobjetivoronda)
+{
+    int acu=0
+    if (dadoapasar > dadostockj)
+    {
+        return 1; // pasar mensaje de que el valor ingresado era incorrecto y ya la cago
+    }
+    if (Vdadosasumar [dadoapasar] == 1)
+    {
+        return 2; // pasar el mensaje de que ese valor ya lo habia ingresado y la cago
+    }
+    else
+    {
+        Vdadosasumar [dadoapasar] = 1;
+    }
+    for (int x=0, x < dadostockj, x++)
+    {
+        if (Vdadosasumar [x] == 1)
+        {
+           acu +=  mj [juego] [turno] [x];
+           if (acu > valorobjetivoronda)
+           {
+               return 3; // pasar el mensaje de que se paso y la cago
+           }
+           else
+           {
+               if (acu == valorobjetivoronda)
+               {
+                   return 5; // pasar el mensaje de que llego al VALOR OBJETIVO
+               }
+               else
+               {
+                   return 4; // pasar el mensaje que todavia no llego al VALOR OBJETIVO
+               }
+           }
+        }
+    }
+}
--- REVENGE/main.cpp
+++ REVENGE/main.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
-#include <conio.h>
 
 using namespace std;
 
 int main()
 {
     ///Variables y constantes
-    int opciones = 0, JUEGOSALM=5  DADOSMAX=11, RONDAS=5, DADOS12=2, contadorjuego=-1, contadorturno = 0, dadonombre1 = 0, dadonombre2 = 0, valorobjetivo = 0, dadostockj1 = 0, dadostockj2 = 0 ;
+    int opciones = 0, JUEGOSALM=5  DADOSMAX=11, RONDAS=5, DADOS12=2, contadorjuego=-1, contadorturno = 0, dadostockj1 = 0, dadostockj2 = 0,valorobjetivoronda = 0;
     char opciones2;
     bool verificador=false, verificadornombre=false;
-    int MtiradasD6J1 [JUEGOSALM][RONDAS][DADOSMAX];   // para almacenar las tiradas de 6 caras del Jugador 1
-    int MtiradasD6J2 [JUEGOSALM][RONDAS][DADOSMAX];   // para almacenar las tiradas de 6 caras del Jugador 2
-    int MtiradasD12 [JUEGOSALM][RONDAS+1][DADOS12];   // para almacenar las tiradas para definir quien inicia y las del valor objetivo
-    int VpuntajeJ1 [JUEGOSALM];                       // para almacenar los puntajes finales del Jugador 1
-    int VpuntajeJ2 [JUEGOSALM];                       // para almacenar los puntajes finales del Jugador 2
+    int MtiradasD6J1 [JUEGOSALM][RONDAS][DADOSMAX] = { };   // para almacenar las tiradas de 6 caras del Jugador 1
+    int MtiradasD6J2 [JUEGOSALM][RONDAS][DADOSMAX]= { };   // para almacenar las tiradas de 6 caras del Jugador 2
+    int MtiradasD12 [JUEGOSALM][RONDAS+1][DADOS12]= { };   // para almacenar las tiradas para definir quien inicia y las del valor objetivo
+    int VpuntajeJ1 [JUEGOSALM]= { };                       // para almacenar los puntajes finales del Jugador 1
+    int VpuntajeJ2 [JUEGOSALM] = { };                       // para almacenar los puntajes finales del Jugador 2
+    int Vdadosasumar [DADOSMAX] = { };                   // para almacenar los dados que va ingresando para pasar
     string nombre1, nombre2, Vjugador1[JUEGOSALM], Vjugador2[JUEGOSALM] ;
     // TODO: poner la semilla del randomizador
     // TODO: ver como usar la libreria conio.h para cambiar colores de las fuentes y el fondo
@@ -57,7 +57,7 @@ int main()
                 cin >> nombre1;
                 cout << endl << "Ingrese el segundo nombre: ";
                 cin >> nombre2;
-                cout << "Los nombres ingresados fueron: " << nombre1 << " y " << nombre2 << ". Son correctos? (S/N): ";
+                cout << endl << endl << "Los nombres ingresados fueron: " << nombre1 << " y " << nombre2 << ". Son correctos? (S/N): ";
                 verificador = false;
                 while (verificador == false)
                 {
@@ -86,7 +86,8 @@ int main()
             cout << endl << "ahora se definira el orden en que jugaran" << endl;
             do
             {
-                system ("pause");
+                cout << endl << "Presione enter para continuar";
+                getchar();
                 contadorturno = 0;
                 verificador = false;
                 lanzardado12c (RONDAS, DADOS12, MtiradasD12, contadorjuego, contadorturno);
@@ -119,35 +120,95 @@ int main()
             contadorturno++
             cout << endl << "################ AHORA COMIENZA EL JUEGO ################";
             /// REPETIR 5 VECES
-                /// mostrar numero de ronda, nombre de quien esta jugando, puntaje acumulado de ambos, cantidad Dados de Stock de ambos
-                cout << endl << "----";
+            for (int i=0, i<RONDAS, i++)
+            {
+                // mostrar numero de ronda, nombre de quien esta jugando, puntaje acumulado de ambos, cantidad Dados de Stock de ambos
+                cout << endl << "-- -- -- -- -- -- -- -- -- --";
                 cout << endl << "Ronda N°: " << contadorturno << endl;
                 cout << Vjugador1 [contadorjuego] << " - puntaje: " << VpuntajeJ1 [contadorjuego] << " - Dados de Stock: " << dadostockj1;
                 cout << Vjugador2 [contadorjuego] << " - puntaje: " << VpuntajeJ2 [contadorjuego] << " - Dados de Stock: " << dadostockj2;
-                cout << endl << "----";
-                /// obtener el Valor Objetivo de la ronda haciendo que el Jugador 1 lance 2D12
+                cout << endl << "-- -- -- -- -- -- -- -- -- --";
+                // obtener el Valor Objetivo de la ronda haciendo que el Jugador 1 lance 2D12
                 cout << endl << Vjugador1 [contadorjuego] << " por favor tira los dados de 12 caras para determinar el valor objetivo de esta ronda";
-                system ("pause"); // o quizas getch ()
+                cout << endl << "Presione enter para continuar";
+                getchar();
+                system ("cls");
                 lanzardado12c (RONDAS, DADOS12, MtiradasD12, contadorjuego, contadorturno);
-                /// agregar o actualizar a lo que se muestra el Valor Objetivo
-                // TODO: ver como usar clrscr para limpiar y reemplazar por el nuevo
-                cout << endl << "----";
+                // agregar o actualizar a lo que se muestra el Valor Objetivo
+                valorobjetivoronda = MtiradasD12 [contadorjuego] [contadorturno] [0] + MtiradasD12 [contadorjuego] [contadorturno] [1]
+                cout << endl << "-- -- -- -- -- -- -- -- -- --";
                 cout << endl << "Ronda N°: " << contadorturno << endl;
                 cout << Vjugador1 [contadorjuego] << " - puntaje: " << VpuntajeJ1 [contadorjuego] << " - Dados de Stock: " << dadostockj1;
                 cout << Vjugador2 [contadorjuego] << " - puntaje: " << VpuntajeJ2 [contadorjuego] << " - Dados de Stock: " << dadostockj2;
-                cout << endl << "salieron : " << MtiradasD12 [contadorjuego] [contadorturno] [0] << " y " << MtiradasD12 [contadorjuego] [contadorturno] [1] << " / VALOR OBJETIVO: " << MtiradasD12 [contadorjuego] [contadorturno] [0] + MtiradasD12 [contadorjuego] [contadorturno] [1];
-                cout << endl << "----";
-                /// lanzar Dados de Stock del Jugador 1
+                cout << endl << "salieron : " << MtiradasD12 [contadorjuego] [contadorturno] [0] << " y " << MtiradasD12 [contadorjuego] [contadorturno] [1] << " / VALOR OBJETIVO: " << valorobjetivoronda;
+                cout << endl << "-- -- -- -- -- -- -- -- -- --";
+                // lanzar Dados de Stock del Jugador 1
                 cout << endl << endl << "Por favor, " << Vjugador1 [contadorjuego] << " lanza tus dados de stock";
-                system ("pause");
+                cout << endl << "Presione enter para continuar";
+                getchar();
                 lanzardado6c (RONDAS, DADOSMAX, MtiradasD6J1, dadostockj1, contadorjuego, contadorturno);
-                /// mostrar valores de los dados
+                // mostrar valores de los dados
                 cout << endl << "resultados: ";
                 for (int x=0, x<dadostockj1, x++)
                 {
                     cout << endl << "dado N° " << x+1 << ": " << MtiradasD6J1 [contadorjuego] [contadorturno] [x];
+                    Vdadosasumar [x] = 0;
                 }
                 /// Dar chance al Jugador 1 que verifique si puede sumar el Valor Objetivo
+                cout << endl << "------";
+                cout << endl << "selecciona que dados utilizaras para llegar al VALOR OBJETIVO, deberas seleccionar de a 1 dado a la vez. Presiona 0 para terminar o pasar" << endl <<"------";
+                for (int y=0, y<dadostockj1, y++)
+                {
+                    cout << endl << "ingrese el numero de dador a sumar: ";
+                    verificador = false;
+                    while (verificador == false)
+                    {
+                      cin >> dadoapasar;
+                       if (isdigit (dadoapasar) == 1)
+                        {
+                           verificador = true;
+                        }
+                        else
+                        {
+                            cout << endl << "No parecia necesario aclararlo, pero solo puede ingresar numeros. Intentelo nuevamente";
+                        }
+                    }
+                    switch (verificadadoapasar(dadostockj1, dadoapasar, Vdadosasumar, RONDAS, DADOSMAX, MtiradasD6J1, contadorjuego, contadorturno, valorobjetivoronda) )
+                    {
+                    case 1:
+                        cout << endl << "no deberias haber ingresado un numero de dado mayor a la cantidad de dados que tenias. PERDISTE EL TURNO";
+                        y = dadostockj1;
+                        for (int r=0, r<dadostockj1, r++)
+                        {
+                            Vdadosasumar [r] = 0
+                        }
+                    break;
+                    case 2:
+                        cout << endl << "no deberias haber ingresado un numero de dado que ya habias utilizado. PERDISTE EL TURNO";
+                        y = dadostockj1;
+                        for (int r=0, r<dadostockj1, r++)
+                        {
+                            Vdadosasumar [r] = 0
+                        }
+                    break;
+                    case 3:
+                        cout << endl << "superaste el Valor Objetivo. PERDISTE EL TURNO";
+                        y = dadostockj1;
+                        for (int r=0, r<dadostockj1, r++)
+                        {
+                            Vdadosasumar [r] = 0;
+                        }
+                    break;
+                    case 4:
+                        cout << endl << "Todavia no llegaste al Valor Objetivo";
+                    break;
+                    case 5:
+                        cout << endl << "Llegaste al Valor Objetivo";
+                        y = dadostockj1;
+                    break;
+                    }
+                }
+
                     ///SI - calcular puntaje
                     ///SI - sumar puntaje
                     ///SI - enviar Dados Elejidos al jugador 2
@@ -164,6 +225,7 @@ int main()
                     ///NO - en caso que el Jugador 1 tenga 2 o mas dados, el jugador 2 recibira un Dado de Stock del jugador 1
                 /// actualizar puntaje acumulado del Jugador 1 y cantidad de Dados de Stock de ambos jugadores
                 /// aumentar contador de ronda
+            }
             /// FIN REPETICION
             break;
             ///2- Mostrar estadisticas
